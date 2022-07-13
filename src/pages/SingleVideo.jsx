2a128879e3e52f1cb7d743a75942a4915f103f85
Carlos <mac@Majortomate.local>
2022-07-12 22:13:49 -0500

    feat: move media folder to components folder, Changes title and favicon

---
+++ src/pages/SingleVideo.jsx
@@ -0,0 +1,13 @@
+import SideBar from "../components/Sidebar"
+
+function SingleVideo(){
+
+  return(
+    <div>
+      <SideBar />
+      <h1>This is a Single Video page</h1>
+    </div>
+  )
+}
+
+export default SingleVideo;
