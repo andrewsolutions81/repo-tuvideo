78d1b800b101e6ffed2ca20f0bd10db4082bbea4
Roy <rhavila789@gmail.com>
2022-07-13 20:59:50 -0500

    feat: add component video introduccion

---
+++ src/components/VideoIntroduccion/index.jsx
@@ -0,0 +1,26 @@
+import './styles.css'
+
+function VideoIntroduccion(){
+    return (
+        <div className="introduccion">
+            <div className="video-container">
+            </div>
+            <div className="introduccion-container">
+                <div className="title-video">
+                    ¿Porqué Make it Real?
+                </div>
+                <div className="video-data">
+                    <span>Make it Real</span> -
+                    <span>500 vistas</span> - 
+                    <span>hace 7 meses</span>
+                </div>
+                <div className="video-description">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, ullam eligendi magni deleniti,
+                    quam eius neque eaque nisi fugiat sunt reiciendis assumenda nam minus, pariatur 
+                    provident est odio sequi. Corrupti?
+                </div>
+            </div>
+        </div>
+    )
+}
+export default VideoIntroduccion;
