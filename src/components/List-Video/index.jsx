d01c6c379828be6a496ee03b4a51297c5ae739c7
Roy <rhavila789@gmail.com>
2022-07-14 10:55:46 -0500

    feat: add component list-videos

---
+++ src/components/List-Video/index.jsx
@@ -0,0 +1,93 @@
+import "./styles.css"
+
+function ListVideo(){
+    return(
+        <div className="listvideos">
+        <div className="scroll-container">
+            <div className="scroll-container__items">
+                <div className="video-item">
+                    <div className="video-item__thumbnail">
+                        <img src="https://colegioprovidencia.cl/provieduca/main/img/no-video.jpg" alt=""/>
+                    </div>
+                    <div className="video-item__details">
+                        <div className="title-video">
+                            <p>Titulo de Video</p>
+                            <div className="video-data">
+                                <p>
+                                    <span>500 vistas</span> -
+                                    <span>hace 7 meses</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="video-item">
+                    <div className="video-item__thumbnail">
+                        <img src="https://colegioprovidencia.cl/provieduca/main/img/no-video.jpg" alt=""/>
+                    </div>
+                    <div className="video-item__details">
+                        <div className="title-video">
+                            <p>Titulo de Video</p>
+                            <div className="video-data">
+                                <p>
+                                    <span>500 vistas</span> -
+                                    <span>hace 7 meses</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="video-item">
+                    <div className="video-item__thumbnail">
+                        <img src="https://colegioprovidencia.cl/provieduca/main/img/no-video.jpg" alt=""/>
+                    </div>
+                    <div className="video-item__details">
+                        <div className="title-video">
+                            <p>Titulo de Video</p>
+                            <div className="video-data">
+                                <p>
+                                    <span>500 vistas</span> -
+                                    <span>hace 7 meses</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="video-item">
+                    <div className="video-item__thumbnail">
+                        <img src="https://colegioprovidencia.cl/provieduca/main/img/no-video.jpg" alt=""/>
+                    </div>
+                    <div className="video-item__details">
+                        <div className="title-video">
+                            <p>Titulo de Video</p>
+                            <div className="video-data">
+                                <p>
+                                    <span>500 vistas</span> -
+                                    <span>hace 7 meses</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="video-item">
+                    <div className="video-item__thumbnail">
+                        <img src="https://colegioprovidencia.cl/provieduca/main/img/no-video.jpg" alt=""/>
+                    </div>
+                    <div className="video-item__details">
+                        <div className="title-video">
+                            <p>Titulo de Video</p>
+                            <div className="video-data">
+                                <p>
+                                    <span>500 vistas</span> -
+                                    <span>hace 7 meses</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default ListVideo;
--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,12 +1,15 @@
 import Channel_Header from '../components/Channel-Header'
 import TabCarousel from '../components/Tab-Carousel'
 import VideoIntroduccion from '../components/VideoIntroduccion'
+import ListVideo from '../components/List-Video'
+
 function Channel (){
     return(
         <div>
             <Channel_Header/>
             <TabCarousel/>
             <VideoIntroduccion/>
+            <ListVideo/>
         </div>
     )
 }
