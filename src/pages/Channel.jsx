5ea1808353c02582251067ff981dadd00cd4b993
Roy <rhavila789@gmail.com>
2022-07-13 20:59:50 -0500

    feat: add component video introduccion

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,10 +1,12 @@
 import Channel_Header from '../components/Channel-Header'
 import TabCarousel from '../components/Tab-Carousel'
+import VideoIntroduccion from '../components/VideoIntroduccion'
 function Channel (){
     return(
         <div>
             <Channel_Header/>
             <TabCarousel/>
+            <VideoIntroduccion/>
         </div>
     )
 }
