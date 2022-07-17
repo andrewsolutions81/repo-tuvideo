fed42f773efa3b02be716101788f36d91c6620f6
Carlos Beltrán R <56098988+majortomate@users.noreply.github.com>
2022-07-17 16:39:10 -0500

    hotfix: solves mayus in name folders, also fixes css mapping (#33)
    Co-authored-by: Carlos <mac@Majortomate.local>

--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,10 +1,12 @@
 import MoreVideo from "../components/MoreVideo"
 import VideoBox from "../components/VideoBox"
+import Header from "../components/Header"
 
 function SingleVideo(){
 
   return(
     <div>
+      <Header />
       <VideoBox />
       <MoreVideo />
     </div>
