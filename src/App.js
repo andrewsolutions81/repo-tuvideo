d518bd58122d402d8499fb110add5ffb1ad492fb
Sergio <sergiomoncadao@gmail.com>
2022-07-16 20:51:16 -0500

    bugfix: fix styles.scss error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SingleVideo from './pages/SingleVideo';
+import Header from './components/header/index.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SingleVideo />
     </div>
   );
