ef799bddbc31b632a4e12afd436620983cdb973e
Carlos Beltrán R <56098988+majortomate@users.noreply.github.com>
2022-07-20 10:31:29 -0500

    fix: add header globally from app, you no longer need to add Header component to each page, now it's global (#43)
    Co-authored-by: Carlos <mac@Majortomate.local>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Channel from './pages/Channel'
 import SingleVideo from './pages/SingleVideo'
+import Header from './components/Header'
 
 
 
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/channel' element={<Channel />} />
--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,12 +1,10 @@
 import MoreVideo from "../components/MoreVideo"
 import VideoBox from "../components/VideoBox"
-import Header from "../components/Header"
 
 function SingleVideo(){
 
   return(
     <div>
-      <Header />
       <VideoBox />
       <MoreVideo />
     </div>
