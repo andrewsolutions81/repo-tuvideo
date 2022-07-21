56c2d856487e9b326c237c48d5a989ce01a7aeeb
Carlos <mac@Majortomate.local>
2022-07-20 22:20:33 -0500

    feat: finish all features about a single video

--- src/components/VideoBox/index.jsx
+++ src/components/VideoBox/index.jsx
@@ -1,115 +1,55 @@
-import  "../VideoBox/styles.scss"
-const videoSamples = [
-  { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
-    "subtitle" : "By Blender Foundation",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
-    "title" : "Big Buck Bunny"
-  },
-  { "description" : "The first Blender Open Movie from 2006",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" ],
-    "subtitle" : "By Blender Foundation",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
-    "title" : "Elephant Dream"
-  },
-  { "description" : "HBO GO now works with Chromecast -- the easiest way to enjoy online video on your TV. For when you want to settle into your Iron Throne to watch the latest episodes. For $35.\nLearn how to use Chromecast with HBO GO and more at google.com/chromecast.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4" ],
-    "subtitle" : "By Google",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
-    "title" : "For Bigger Blazes"
-  },
-  { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when Batman's escapes aren't quite big enough. For $35. Learn how to use Chromecast with Google Play Movies and more at google.com/chromecast.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4" ],
-    "subtitle" : "By Google",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerEscapes.jpg",
-    "title" : "For Bigger Escape"
-  },
-  { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV. For $35.  Find out more at google.com/chromecast.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4" ],
-    "subtitle" : "By Google",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerFun.jpg",
-    "title" : "For Bigger Fun"
-  },
-  { "description" : "Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for the times that call for bigger joyrides. For $35. Learn how to use Chromecast with YouTube and more at google.com/chromecast.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" ],
-    "subtitle" : "By Google",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerJoyrides.jpg",
-    "title" : "For Bigger Joyrides"
-  },
-  { "description" :"Introducing Chromecast. The easiest way to enjoy online video and music on your TV—for when you want to make Buster's big meltdowns even bigger. For $35. Learn how to use Chromecast with Netflix and more at google.com/chromecast.", 
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" ],
-    "subtitle" : "By Google",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerMeltdowns.jpg",
-    "title" : "For Bigger Meltdowns"
-  },
-{ "description" : "Sintel is an independently produced short film, initiated by the Blender Foundation as a means to further improve and validate the free/open source 3D creation suite Blender. With initial funding provided by 1000s of donations via the internet community, it has again proven to be a viable development model for both open 3D technology as for independent animation film.\nThis 15 minute film has been realized in the studio of the Amsterdam Blender Institute, by an international team of artists and developers. In addition to that, several crucial technical and creative targets have been realized online, by developers and artists and teams all over the world.\nwww.sintel.org",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4" ],
-    "subtitle" : "By Blender Foundation",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg",
-    "title" : "Sintel"
-  },
-{ "description" : "Smoking Tire takes the all-new Subaru Outback to the highest point we can find in hopes our customer-appreciation Balloon Launch will get some free T-shirts into the hands of our viewers.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4" ],
-    "subtitle" : "By Garage419",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/SubaruOutbackOnStreetAndDirt.jpg",
-    "title" : "Subaru Outback On Street And Dirt"
-  },
-{ "description" : "Tears of Steel was realized with crowd-funding by users of the open source 3D creation tool Blender. Target was to improve and test a complete open and free pipeline for visual effects in film - and to make a compelling sci-fi film in Amsterdam, the Netherlands.  The film itself, and all raw material used for making it, have been released under the Creatieve Commons 3.0 Attribution license. Visit the tearsofsteel.org website to find out more about this, or to purchase the 4-DVD box with a lot of extras.  (CC) Blender Foundation - http://www.tearsofsteel.org", 
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4" ],
-    "subtitle" : "By Blender Foundation",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg",
-    "title" : "Tears of Steel"
-  },
-{ "description" : "The Smoking Tire heads out to Adams Motorsports Park in Riverside, CA to test the most requested car of 2010, the Volkswagen GTI. Will it beat the Mazdaspeed3's standard-setting lap time? Watch and see...",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4" ],
-    "subtitle" : "By Garage419",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg",
-    "title" : "Volkswagen GTI Review"
-  },
-{ "description" : "The Smoking Tire is going on the 2010 Bullrun Live Rally in a 2011 Shelby GT500, and posting a video from the road every single day! The only place to watch them is by subscribing to The Smoking Tire or watching at BlackMagicShine.com",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4" ],
-    "subtitle" : "By Garage419",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg",
-    "title" : "We Are Going On Bullrun"
-  },
-{ "description" : "The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.The Smoking Tire meets up with Chris and Jorge from CarsForAGrand.com to see just how far $1,000 can go when looking for a car.",
-    "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4" ],
-    "subtitle" : "By Garage419",
-    "thumb" : "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ElephantsDream.jpg",
-    "title" : "What care can you get for a grand?"
-  }
-]
+import MoreVideos from "../MoreVideos";
+import "../VideoBox/styles.scss";
 
-function VideoBox(){
+function VideoBox(props){
+  
+  const { videos, id} = props;
   return(
     <div>
-      <div>
-        <video className="video-box" width="100%" poster={videoSamples[0].thumb} controls>
-          <source src={videoSamples[0].sources} type="video/mp4" />
+      { 
+      videos.map((video) => (video.id + '-' + video.title.toLowerCase().replaceAll(' ', '-') === id ?
+        <div key={video.id}>
+        <video className="video-box" width="100%" poster={video.thumb} controls>
+          <source src={video.sources} type="video/mp4" />
         </video>
-      </div>
-    <div className="video-options">
-        <h1 className="video-options__title">{videoSamples[0].title}</h1>
-        <div className="video-options__container">
+        <div className="video-options">
+          <h1 className="video-options__title">{video.title}</h1>
+          <div className="video-options__container">
             <p className="video-options__views">3.000.000 views</p>
             <div className="video-options__buttons">
-                <button type="button" className="video-options__like-btn"><img src="/media/icons/Mylikes.png" alt="like"/>60.000</button>
-                <button type="button" className="video-options__dislike-btn"><img src="/media/icons/dont-like.png" alt="dislike"/>Don't like it</button>
-                <button type="button" className="video-options__share-btn"><img src="/media/icons/share.png" alt="share"/>Share</button>
+              <button type="button" className="video-options__like-btn">
+                <img src="/media/icons/Mylikes.png" alt="like" />
+                60.000
+              </button>
+              <button type="button" className="video-options__dislike-btn">
+                <img src="/media/icons/dont-like.png" alt="dislike" />
+                Don't like it
+              </button>
+              <button type="button" className="video-options__share-btn">
+                <img src="/media/icons/share.png" alt="share" />
+                Share
+              </button>
             </div>
+          </div>
         </div>
-    </div>
-    <div className="ch-info-container">
-        <div className="ch-info-container__avatar"><img src="/media/images/ch-avatar.jpeg" alt="avatar"/></div>
-        <div className="ch-info-container__ch-info">
-            <h2 className="ch-info-container__name">{videoSamples[0].subtitle}</h2>
-            <p className="ch-info-container__subs">45.567 subscribers</p>
-            <p className="ch-info-container__desc">{videoSamples[0].description}</p>
-        </div>
-        <div className="ch-info-container__subs-btn">
+        {/* Channel info and video description */}
+        <div className="ch-info-container">
+          <div className="ch-info-container__avatar">
+            <img src="/media/images/ch-avatar.jpeg" alt="avatar" />
+          </div>
+          <div className="ch-info-container__ch-info">
+            <h2 className="ch-info-container__name">{video.subtitle}</h2>
+            <p className="ch-info-container__subs">{`${new Intl.NumberFormat().format(Math.floor(Math.random() * 100000))} subscribers`}</p>
+            <p className="ch-info-container__desc">{video.description}</p>
+          </div>
+          <div className="ch-info-container__subs-btn">
             <button type="button">Subscribe</button>
+          </div>
         </div>
-    </div>
+      <MoreVideos video={video} videos={videos}/>
+        </div>       
+      :null))
+      }
     </div>
   )
 }
