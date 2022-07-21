b81e8933105e2dfd34b16d1fc4afe7ac8e14ca1c
Carlos Beltrán R <56098988+majortomate@users.noreply.github.com>
2022-07-21 07:17:59 -0500

    Feat/finish single video feat (#46)
    * fix: add header globally from app, you no longer need to add Header component to each page, now it's global
    
    * hotfix: fixes sidebar completely
    
    * feat: finish all features about a single video
    
    * feat: add page title for every video
    
    Co-authored-by: Carlos <mac@Majortomate.local>

---
+++ src/services/videos.js
@@ -0,0 +1,12 @@
+import data from '../assets/data/videos.json';
+
+export function getVideos() {
+  return data;
+}
+
+export function getVideo(id) {
+  return data.find(video => video.id === id);
+}
+export function getVideoByCategory(category) {
+  return data.find(video => video.category === category);
+}
