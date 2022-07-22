d6aed5d3e8fd841531ef6497db4b0ac07a54fa24
Carlos <mac@Majortomate.local>
2022-07-21 22:57:25 -0500

    feat: add all states for every input and also to save form as state. Next step: publish video

--- src/components/UploadForm/index.jsx
+++ src/components/UploadForm/index.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react"
+
 function UploadForm(props){
   const {file} = props
+  
+  const [saveChannel, setSaveChannel] = useState("")
+  const [saveTitle, setSaveTitle] = useState("")
+  const [saveCategory, setSaveCategory] = useState("")
+  const [saveDescription, setSaveDescription] = useState("")
+  const [saveForm, setSaveForm] = useState([])
+  
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const list = [...saveForm, {"Channel":saveChannel, "Title":saveTitle, "Category":saveCategory, "Description":saveDescription, "id":Math.floor(Math.random() * 10000)}]
+    setSaveForm(list)
+      //  setText("")
+  }
+
   return(
     <div>
       {
         file ? 
-        <form className="upload-form">
+        <form className="upload-form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="video-channel-name">What is your channel name</label>
-            <input type="text" id="video-channel-name" placeholder="Channel name"/>
+            <input value={saveChannel} type="text" id="video-channel-name" placeholder="Channel name" onChange={(e)=> setSaveChannel(e.target.value)}/>
           </div>
           <div>
             <label htmlFor="select-category">What category suits your video better
-              <select id="select-category">
+              <select id="select-category" onChange={(e)=> setSaveCategory(e.target.value)}>
                 <option value="fun">Fun</option>
                 <option value="music">Music</option>
                 <option value="terror">Terror</option>
@@ -20,19 +37,18 @@ function UploadForm(props){
           </div>
           <div>
             <label htmlFor="video-title-name">What title you want for your video</label>
-            <input type="text" id="video-title-name" placeholder="Write a meaningfull title"/>
+            <input value={saveTitle} type="text" id="video-title-name" placeholder="Write a meaningfull title" onChange={(e)=> setSaveTitle(e.target.value)}/>
           </div>
           <div>
             <label htmlFor="video-desc-name">What description you want for your video</label>
-            <textarea type="text" id="video-desc-name"  rows="7"/>
+            <textarea value={saveDescription} type="text" id="video-desc-name"  rows="7" onChange={(e)=> setSaveDescription(e.target.value)}/>
           </div>
           <div className="upload__button">
-            <button type="button">UPLOAD VIDEO</button>      
+            <button type="submit">UPLOAD VIDEO</button>      
           </div>
         </form> :
         null
       }
-
     </div>
   )
 }
