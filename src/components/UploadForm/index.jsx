f3106f2ae9d2d05ea949f466bf2d1dd4008f6343
Carlos <mac@Majortomate.local>
2022-07-22 10:49:27 -0500

    fix: refactor handlechange and handlesubmit

--- src/components/UploadForm/index.jsx
+++ src/components/UploadForm/index.jsx
@@ -6,22 +6,23 @@ import {useNavigate} from 'react-router-dom'
 function UploadForm(props){
   const {file} = props
   const navigate = useNavigate();
-  const [saveChannel, setSaveChannel] = useState("")
-  const [saveTitle, setSaveTitle] = useState("")
-  const [saveCategory, setSaveCategory] = useState("")
-  const [saveDescription, setSaveDescription] = useState("")
+  const [data, setData] = useState({})
   const [saveForm, setSaveForm] = useState([])
   const [isSent, setIsSent] = useState(false)
   
-
+  function handleChange(e){
+    const {name, value} = e.target
+    setData({...data, [name]:value})
+  }
   function handleSubmit(e){
     e.preventDefault()
-    const list = [...saveForm, {"channel":saveChannel, "title":saveTitle, "category":saveCategory, "description":saveDescription, "sources":file, "id":Math.floor(Math.random() * 10000)}]
+    const list = {
+      ...data,
+      id: Math.floor(Math.random() *1000),
+      sources: file
+    }
     setSaveForm(list)
-    setSaveChannel("")
-    setSaveTitle("")
-    setSaveCategory("")
-    setSaveDescription("")
+    setData("")
     setIsSent(true)
     //navigate('/:id', {state:list});
 
@@ -34,11 +35,11 @@ function UploadForm(props){
         <form className="upload-form" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="video-channel-name">What is your channel name</label>
-            <input value={saveChannel} type="text" id="video-channel-name" placeholder="Channel name" onChange={(e)=> setSaveChannel(e.target.value)}/>
+            <input type="text" id="video-channel-name" name="channel" placeholder="Channel name" onChange={handleChange}/>
           </div>
           <div>
             <label htmlFor="select-category">What category suits your video better
-              <select id="select-category" onChange={(e)=> setSaveCategory(e.target.value)}>
+              <select id="select-category" name="category" onChange={handleChange}>
                 <option value="select">Select</option>
                 <option value="fun">Fun</option>
                 <option value="music">Music</option>
@@ -48,11 +49,11 @@ function UploadForm(props){
           </div>
           <div>
             <label htmlFor="video-title-name">What title you want for your video</label>
-            <input value={saveTitle} type="text" id="video-title-name" placeholder="Write a meaningfull title" onChange={(e)=> setSaveTitle(e.target.value)}/>
+            <input type="text" id="video-title-name" name="title" placeholder="Write a meaningfull title" onChange={handleChange}/>
           </div>
           <div>
             <label htmlFor="video-desc-name">What description you want for your video</label>
-            <textarea value={saveDescription} type="text" id="video-desc-name"  rows="7" onChange={(e)=> setSaveDescription(e.target.value)}/>
+            <textarea type="text" id="video-desc-name" name="description"  rows="7" onChange={handleChange}/>
           </div>
           <div className="upload__button">
             <button type="submit">UPLOAD VIDEO</button>      
