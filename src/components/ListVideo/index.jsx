28abbc859390b6674acc44ba39d5bf67b12c8540
Roy <rhavila789@gmail.com>
2022-08-05 00:42:05 -0500

    feat: carousel videos and add routes

--- src/components/ListVideo/index.jsx
+++ src/components/ListVideo/index.jsx
@@ -6,35 +6,37 @@ import VideoItem from '../VideoItem';
 
 function VideoCarousel() {
   const [videos, setVideos] = useState([]);
-  const [styleScroll, setStyleScroll] = useState({});
-  const [amount, setAmount] = useState(0);
+  const [move, setMove] = useState(0);
+  const [style, setStyle] = useState({ transform: 'translateX(0px)' });
 
-  const handleClick = () => {
-    const container = document.querySelector('.list-videos');
-    const totalWidth = videos.length * 180;
-    const translate = container.clientWidth;
-    const restante = totalWidth - amount;
-
-    console.log('totalWidth: ', totalWidth);
-    console.log('translate: ', translate);
-    console.log('restante: ', restante);
-    console.log('amountTranslate:', amount);
-
-    if (restante >= translate) {
-      setAmount(amount + translate);
-      const newStyle = { transform: `translate(-${amount}px)` };
-      setStyleScroll(newStyle);
+  const handleClickRight = () => {
+    const width = document.querySelector('.list-videos').clientWidth;
+    const maxWidth = videos.length * 180;
+    const limit = maxWidth - width;
+    const restWidth = limit - move;
+    if (restWidth > width) {
+      setMove(move + width);
     } else {
-      const newStyle = { transform: `translate(-${amount + restante}px)` };
-      setStyleScroll(newStyle);
+      setMove(limit);
     }
-    /* const newStyle = { transform: `translate(-${width - 10}px)` };
-    setWidth(container.clientWidth);
-    setStyleScroll(newStyle);
-    console.log(width);
-    console.log(maxWidth); */
+    setMove((state) => {
+      setStyle({ transform: `translateX(-${state}px)` });
+      return state;
+    });
   };
 
+  const handleClickLeft = () => {
+    const width = document.querySelector('.list-videos').clientWidth;
+    if (move > 0 && move > width) {
+      setMove(move - width);
+    } else {
+      setMove(0);
+    }
+    setMove((state) => {
+      setStyle({ transform: `translateX(-${state}px)` });
+      return state;
+    });
+  };
   useEffect(() => {
     const result = getVideos();
     setVideos(result);
@@ -53,15 +55,20 @@ function VideoCarousel() {
         </list>
       </div>
       <div className="list-videos">
-        <div className="scroll-container" style={styleScroll}>
+        <div className="scroll-container" style={style}>
           {
             videos.map((video) => <VideoItem key={video.id} video={video} />)
           }
         </div>
       </div>
-      <button type="button" className="arrow" onClick={handleClick}>
+      <button type="button" className="arrow-right" onClick={handleClickRight}>
         <svg height="40" width="40">
           <path d="M9.4,18.4l-0.7-0.7l5.6-5.6L8.6,6.4l0.7-0.7l6.4,6.4L9.4,18.4z" />
+        </svg>
+      </button>
+      <button type="button" className="arrow-left" onClick={handleClickLeft}>
+        <svg height="40" width="40">
+          <path d="M14.6,18.4L8.3,12l6.4-6.4l0.7,0.7L9.7,12l5.6,5.6L14.6,18.4z" />
         </svg>
       </button>
     </div>
