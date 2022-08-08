e747add17691e93ea559cc4e84ed28cc09f7594a
Carlos <mac@Majortomate.local>
2022-08-07 23:41:44 -0500

    feat: finish upload function and add video to database

--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import VideoBox from '../components/VideoBox';
 import { getVideos } from '../services/videos';
@@ -6,10 +6,26 @@ import { getVideos } from '../services/videos';
 function SingleVideo() {
   const { id } = useParams();
   const [videos, setVideos] = useState(getVideos());
+  const [video, setVideo] = useState({});
+
+  useEffect(() => {
+    fetch(`https://tuvideo-backend.herokuapp.com/api/videos/${id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        if (res) {
+          setVideo(res);
+        } else {
+          console.log('error');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
-      <VideoBox videos={videos} setVideos={setVideos} id={id} />
+      <VideoBox videos={videos} setVideos={setVideos} id={id} video={video} />
     </div>
   );
 }
