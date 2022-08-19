9cedb91af8795254e0e9b197c2eeb42160218706
Carlos <mac@Majortomate.local>
2022-08-18 21:41:27 -0500

    feat: finish details on upload video page

--- src/components/FileUploader/index.jsx
+++ src/components/FileUploader/index.jsx
@@ -1,52 +1,66 @@
+/* eslint-disable object-shorthand */
 /* eslint-disable no-shadow */
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useState } from 'react';
+import axios from 'axios';
 import UploadForm from '../UploadForm';
-// import UploadingSpinner from '../UploadingSpinner';
 import '../UploadVideo/styles.scss';
+import UploadingSpinner from '../UploadingSpinner';
 
 function VideoUploader() {
   const [file, setFile] = useState('');
-  const [url, setUrl] = useState('');
-
-  const uploadVideoDetails = (video) => {
-    const data = new FormData();
-    data.append('file', video);
-    data.append('upload_preset', 'tuvideo-video');
-    data.append('cloud_name', 'dqv35f3nq');
-    fetch('https://api.cloudinary.com/v1_1/dqv35f3nq/video/upload', {
-      method: 'post',
-      body: data,
-    })
-      .then((res) => res.json())
-      .then((newdata) => {
-        setUrl(newdata.secure_url);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  const [videoUrl, setVideoUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showFileInput, setShowFileInput] = useState(true);
 
   const handleChange = (e) => {
-    const fileData = e.target.files[0];
-    setFile(fileData);
-    setUrl(uploadVideoDetails(fileData));
+    setFile(e.target.files[0]);
+  };
+
+  const handleUploadFile = async () => {
+    setLoading(true);
+    const formData = new FormData();
+
+    formData.append('file', file);
+    formData.append('filename', file.name);
+
+    try {
+      const data = await axios.post('http://localhost:3001/api/upload/file', formData);
+      setVideoUrl(data.data.secure_url);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <div>
-      <div className="file-input">
-        <img src="/media/icons/uploading.png" alt="" />
-        <label htmlFor="myfile">Select a video:</label>
-        <p>{file ? `File name: ${file.name} ` : 'no files uploaded yet'}</p>
-        <input type="file" accept="video/*" id="myfile" style={{ opacity: '0' }} onChange={handleChange} />
-      </div>
-      <div>
-        {url && <video width="100%" src={`${url}#t=5`} poster={url} controls />}
-      </div>
-      <UploadForm cloudinary={url} />
-    </div>
+    <>
+      {
+      showFileInput
+        ? (
+          <div>
+            <div className="file-input">
+              <img src="/media/icons/uploading.png" alt="" />
+              <label htmlFor="myfile">Select a video:</label>
+              <p>{file ? `File name: ${file.name} ` : 'no files uploaded yet'}</p>
+              <input type="file" accept="video/*" id="myfile" style={{ opacity: '0' }} onChange={handleChange} />
+              {
+          file && <button type="button" onClick={handleUploadFile}>Upload</button>
+        }
+            </div>
+            {loading ? (
+              <>
+                <UploadingSpinner />
+                {' '}
+                <h2>Uploading Video...</h2>
+              </>
+            ) : null}
+          </div>
+        ) : null
+    }
+      <UploadForm cloudinary={videoUrl} setShowFileInput={setShowFileInput} />
+    </>
   );
 }
 export default VideoUploader;
