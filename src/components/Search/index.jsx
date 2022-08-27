75c4c14557e10c51cfadb5652635fadbd574ba6e
Carlos Beltrán R <56098988+majortomate@users.noreply.github.com>
2022-08-27 12:18:48 -0500

    Feat/improve single video (#94)

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,23 +2,24 @@
 /* eslint-disable array-callback-return */
 /* eslint-disable consistent-return */
 /* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './styles.scss';
 import axios from 'axios';
 import SearchItem from '../SearchItem';
 import useDebounce from '../../hooks/useDebounce';
+import { SearchContext } from '../../searchContext/SearchContext';
 
 function Search() {
   const [allVideos, setAllVideos] = useState([]);
-  const [query, setQuery] = useState('');
   const [close, setClose] = useState(false);
+  const { dispatch, state } = useContext(SearchContext);
 
   const handleChange = (e) => {
-    setQuery(e.target.value.toLowerCase());
+    dispatch({ type: 'typing', payload: e.target.value.toLowerCase() });
     setClose(false);
   };
 
-  const debounceSearch = useDebounce(query, 500);
+  const debounceSearch = useDebounce(state.query, 500);
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -30,10 +31,7 @@ function Search() {
   return (
     <div>
       <form action="" className="search-form">
-        <input type="search" placeholder="Search..." onChange={handleChange} value={query} />
-        <div className="header__search-bar__icon">
-          <img src="/media/icons/Search.png" alt="Busca" />
-        </div>
+        <input type="search" placeholder="Type something..." onChange={handleChange} value={state.query} />
       </form>
       {
         debounceSearch
--- src/pages/SingleVideo.jsx
+++ src/pages/SingleVideo.jsx
@@ -1,28 +1,14 @@
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import VideoBox from '../components/VideoBox';
+import useFetch from '../hooks/useFetch';
 
 function SingleVideo() {
   const { id } = useParams();
-  const [video, setVideo] = useState({});
 
-  useEffect(() => {
-    fetch(`https://tuvideo-backend.herokuapp.com/api/videos/${id}`)
-      .then((res) => res.json())
-      .then((res) => {
-        if (res) {
-          setVideo(res);
-        } else {
-          console.log('error');
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
+  const video = useFetch('https://tuvideo-backend.herokuapp.com/api/videos/', id);
 
   return (
-    <div>
+    <div className="container">
       <VideoBox id={id} video={video} />
     </div>
   );
