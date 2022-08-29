08c31b6a2f25d2a0f03c6dc96cded20e5cb8e197
Sergio Moncada <105378382+Feelgood3@users.noreply.github.com>
2022-08-29 08:32:59 -0500

    Feat/register reform redux (#101)
    * feat/reformulating register component + action, now working
    
    * package-lock verify

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -2,11 +2,8 @@
 /* eslint-disable no-shadow */
 /* eslint-disable consistent-return */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-import CheckButton from 'react-validation/build/button';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { isEmail } from 'validator';
 import { register } from '../../actions/auth';
 
@@ -47,40 +44,22 @@ const vpassword = (value) => {
   }
 };
 function Register() {
-  const form = useRef();
-  const checkBtn = useRef();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [successful, setSuccessful] = useState(false);
-  const { message } = useSelector((state) => state.message);
+  const [form, setForm] = useState({});
   const dispatch = useDispatch();
-  const onChangeUsername = (e) => {
-    const username = e.target.value;
-    setUsername(username);
+
+  const onHandleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
   };
-  const onChangeEmail = (e) => {
-    const email = e.target.value;
-    setEmail(email);
-  };
-  const onChangePassword = (e) => {
-    const password = e.target.value;
-    setPassword(password);
-  };
+
   const handleRegister = (e) => {
     e.preventDefault();
-    setSuccessful(false);
-    form.current.validateAll();
-    if (checkBtn.current.context._errors.length === 0) {
-      dispatch(register(username, email, password))
-        .then(() => {
-          setSuccessful(true);
-        })
-        .catch(() => {
-          setSuccessful(false);
-        });
-    }
+    const { username, password, email } = form;
+    dispatch(register(username, password, email));
   };
+
   return (
     <div className="col-md-12">
       <div className="card card-container">
@@ -89,56 +68,49 @@ function Register() {
           alt="profile-img"
           className="profile-img-card"
         />
-        <Form onSubmit={handleRegister} ref={form}>
-          {!successful && (
-            <div>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <Input
+        <form onSubmit={handleRegister}>
+          <div>
+            <div className="form-group">
+              <label htmlFor="username">
+                Username
+                <input
                   type="text"
                   className="form-control"
                   name="username"
-                  value={username}
-                  onChange={onChangeUsername}
+                  id={vusername}
+                  onChange={onHandleChange}
                   validations={[required, vusername]}
                 />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  className="form-control"
-                  name="email"
-                  value={email}
-                  onChange={onChangeEmail}
-                  validations={[required, validEmail]}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  value={password}
-                  onChange={onChangePassword}
-                  validations={[required, vpassword]}
-                />
-              </div>
-              <div className="form-group">
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-              </div>
+              </label>
             </div>
-          )}
-          {message && (
             <div className="form-group">
-              <div className={successful ? 'alert alert-success' : 'alert alert-danger'} role="alert">
-                {message}
-              </div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id={validEmail}
+                onChange={onHandleChange}
+                validations={[required, validEmail]}
+              />
             </div>
-          )}
-          <CheckButton style={{ display: 'none' }} ref={checkBtn} />
-        </Form>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                name="password"
+                id={validEmail}
+                onChange={onHandleChange}
+                validations={[required, vpassword]}
+              />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+            </div>
+          </div>
+          <input type="checkbox" name="remember" id="remember" />
+        </form>
       </div>
     </div>
   );
