bb91e85d382e3a5861efa97b952de6b75eb51a04
Roy <rhavila789@gmail.com>
2022-08-31 15:43:37 -0500

    fix: fix styles

--- src/components/ChannelHeader/index.jsx
+++ src/components/ChannelHeader/index.jsx
@@ -90,7 +90,7 @@ function ChannelHeader() {
                       }}
                     />
                     <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="camera-icon">
-                      <g className="style-scope yt-icon">
+                      <g className="ge">
                         <path d="M12,10c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,10,12,10 M12,9c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,9,12,9L12,9z M14.59,5l1.71,1.71L16.59,7H17h4v12H3V7h4h0.41l0.29-0.29L9.41,5H14.59 M15,4H9L7,6H2v14h20V6h-5L15,4L15,4z" className="style-scope yt-icon" stroke="white" />
                       </g>
                     </svg>
@@ -118,7 +118,7 @@ function ChannelHeader() {
                   }}
                 />
                 <svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" className="camera-icon">
-                  <g className="style-scope yt-icon">
+                  <g className="ge">
                     <path d="M12,10c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,10,12,10 M12,9c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4 S14.21,9,12,9L12,9z M14.59,5l1.71,1.71L16.59,7H17h4v12H3V7h4h0.41l0.29-0.29L9.41,5H14.59 M15,4H9L7,6H2v14h20V6h-5L15,4L15,4z" className="style-scope yt-icon" stroke="white" />
                   </g>
                 </svg>
